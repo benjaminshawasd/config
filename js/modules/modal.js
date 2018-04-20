b94d4d3add3d49021d7bc9eb03090ebee557e7df
iClippy <iClippy@outlook.com>
2018-04-20 13:25:28 +0200

    Uses const instead of var in modals.js code

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,14 +1,14 @@
 $('.fluent-modal').on({
 
     'shown.bs.modal': function(e){
-        var noBackdrop = !document.querySelector(".modal-backdrop");
+        const noBackdrop = !document.querySelector(".modal-backdrop");
         if(noBackdrop) {
             $(this).addClass("modal-no-backdrop");
         }
     },
 
     'hide.bs.modal': function(e){
-        var noBackdrop = !document.querySelector(".modal-backdrop");
+        const noBackdrop = !document.querySelector(".modal-backdrop");
         if(noBackdrop) {
             $(this).removeClass("modal-no-backdrop");
         }
